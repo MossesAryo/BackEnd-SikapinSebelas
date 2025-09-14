ca6bcea2bfd57eca25d0bb45d94949baf7476ba0
Mosses Aryo Bimo <mossesaryobimo@gmail.com>
2025-09-14 16:22:03 +0700

    add API (#2)

--- app/Http/Controllers/catatanController.php
+++ app/Http/Controllers/catatanController.php
@@ -27,4 +27,27 @@ class catatanController extends Controller
 
        return redirect()->back()->with('success', 'Catatan   berhasil ditambahkan');
     }
+    public function AddCatatanAPI(Request $request, $nis)
+{
+    $user = Auth::user();
+    $request->validate([
+        'judul_catatan' => 'required|string',
+        'isi_catatan'   => 'required|string',
+    ]);
+
+    $catatan = catatan::create([
+        'nis'           => $nis,
+        'judul_catatan' => $request->judul_catatan,
+        'isi_catatan'   => $request->isi_catatan,
+        'nip_wakasek'   => $user->wakasek->nip_wakasek ?? null,
+        'nip_walikelas' => $user->walikelas->nip_walikelas ?? null,
+    ]);
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Catatan berhasil ditambahkan',
+        'data'    => $catatan
+    ], 201);
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\catatanController;
 use App\Http\Controllers\AkumulasiContoller;
 use App\Http\Controllers\PenghargaanController;
 use App\Http\Controllers\api\SkoringPelanggaran;
@@ -11,6 +12,7 @@ use App\Http\Controllers\api\SkoringPenghargaan;
 use App\Http\Controllers\Aspek_penilaianController;
 use App\Http\Controllers\AuthAPI\AuthAPIcontroller;
 use App\Http\Controllers\SuratPeringatanController;
+
 
 Route::get('/kelas', [KelasController::class, 'FetchApi'])->name('api.kelas');
 Route::get('/aspekpenilaian', [Aspek_penilaianController::class, 'FetchApi'])->name('api.aspek');
@@ -22,5 +24,6 @@ Route::get('/skoring_pelanggaran', [SkoringPelanggaran::class, 'index']);
 Route::get('/akumulasi', [AkumulasiContoller::class, 'fetchAPI']);
 Route::post('/skoring_penghargaan', [SkoringPenghargaan::class, 'store']);
 Route::post('/skoring_pelanggaran', [SkoringPelanggaran::class, 'store']);
+Route::post('/AddCatatan', [catatanController::class, 'AddCatatanAPI']);
 Route::post('/login', [AuthAPIcontroller::class, 'login']);
 Route::post('/logout', [AuthAPIcontroller::class, 'logout']);
