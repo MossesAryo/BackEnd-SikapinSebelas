233fae8172eeb3c8f69e478a60c28a1e27197ad4
Mosses Aryo Bimo <mossesaryobimo@gmail.com>
2025-10-15 22:31:54 +0700

    FIX : Import Error at Walikelas

--- app/Imports/Walikelas_Import.php
+++ app/Imports/Walikelas_Import.php
@@ -2,20 +2,40 @@
 
 namespace App\Imports;
 
+use App\Models\User;
 use App\Models\Walikelas;
+use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 class Walikelas_Import implements ToModel, WithHeadingRow
 {
+  public function headingRow(): int
+  {
+    return 2;
+  }
   public function model(array $row)
-{
-    return new Walikelas([
-        'nip_walikelas' => $row['nip_walikelas'],
-        'username'      => $row['username'],
-        'nama_walikelas'=> $row['username'], 
-        'id_kelas'      => $row['id_kelas'],
-    ]);
-}
+  {
+    \Illuminate\Support\Facades\Log::info('Processing row: ' . json_encode($row));
+    if (empty($row['username']) || is_null($row['username'])) {
+        \Illuminate\Support\Facades\Log::warning('Null or empty username in row: ' . json_encode($row));
+        return null;
+    } 
 
+    $user = User::Create(
+      [
+        'username' => $row['username'],
+        'email' => $row['username'] . '@gmail.com',
+        'password' => Hash::make('password123'),
+        'role' => 3,
+      ],
+    );
+    
+    return new Walikelas([
+      'nip_walikelas' => $row['nip'],
+      'username'      => $user->username,
+      'nama_walikelas' => $row['nama_walikelas'],
+      'id_kelas'      => $row['id_kelas'],
+    ]);
+  }
 }
