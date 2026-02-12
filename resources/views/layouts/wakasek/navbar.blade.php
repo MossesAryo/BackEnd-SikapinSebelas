14ee09fd6c6475d93eebbc5a1ae6ae6a4c56aa34
MossesAryo <mossesaryobimo@gmail.com>
2026-02-12 19:13:00 +0700

    FIX : Role

--- resources/views/layouts/wakasek/navbar.blade.php
+++ resources/views/layouts/wakasek/navbar.blade.php
@@ -16,9 +16,9 @@
                          {{ strtoupper(substr(Auth::user()->gurubk->nama_guru_bk, 0, 1)) }}
                      @elseif (auth()->user()->role == 1)
                          {{ strtoupper(substr(Auth::user()->wakasek->nama_wakasek, 0, 1)) }}
-                     @elseif (auth()->user()->role == 3)
+                     @elseif (auth()->user()->role == 4)
                          {{ strtoupper(substr(Auth::user()->ketua_program->nama_ketua_program, 0, 1)) }}
-                        @elseif (auth()->user()->role == 4)
+                        @elseif (auth()->user()->role == 3)
                             {{ strtoupper(substr(Auth::user()->walikelas->nama_walikelas, 0, 1)) }}
                      @endif
                  </div>
