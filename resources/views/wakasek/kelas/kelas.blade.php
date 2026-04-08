9f7179707c73b98b326e75fa4ed3a61afbf3bb6c
MossesAryo <mossesaryobimo@gmail.com>
2026-04-09 03:50:00 +0700

    tes dd

--- resources/views/wakasek/kelas/kelas.blade.php
+++ resources/views/wakasek/kelas/kelas.blade.php
@@ -113,6 +113,7 @@
                         </tr>
                     </thead>
                     <tbody id="tableBody" class="bg-white divide-y divide-gray-100">
+                        @dd($kelas)
                         @forelse ($kelas as $item)
                             <tr class="hover:bg-gray-50 group">
                                 <td class="px-6 py-4">
