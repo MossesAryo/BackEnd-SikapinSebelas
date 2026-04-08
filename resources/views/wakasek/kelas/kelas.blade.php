be261657d53b84470436f1a2648481377bc64b45
FajarSidiq08 <sidiqfajar491@gmail.com>
2026-04-08 22:55:06 +0700

    Fix error

--- resources/views/wakasek/kelas/kelas.blade.php
+++ resources/views/wakasek/kelas/kelas.blade.php
@@ -125,9 +125,9 @@
                                     </div>
                                 </td>
                                 <td class="px-6 py-4">
-                                    <div class="text-sm font-semibold text-gray-900">{{ $item->nama_jurusan}}</div>
+                                    <div class="text-sm font-semibold text-gray-900">{{ $item->jurusan->id_jurusan}}</div>
                                 </td>
-                                <td class="px-6 py-4">  
+                                <td class="px-6 py-4">
                                     <div class="text-sm font-semibold text-gray-900">{{ $item->nama_kelas }}</div>
                                 </td>
                                 @if (auth()->user()->role == 1)
